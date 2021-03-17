bfa6638842fb8d0aae85a73237b0743b05bdf9c9
unknown <nellysherova@mail.ru>
2021-03-17 17:58:13 +0300

    Добавила решение 1 таска

--- TryCatchFinallyHW/Program.cs
+++ TryCatchFinallyHW/Program.cs
@@ -1,12 +1,19 @@
 ﻿using System;
-
+using System.Linq;
 namespace TryCatchFinallyHW
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Task 1");
+            Console.ResetColor();
+            Console.Write("Enter text: "); string text = Console.ReadLine();
+            var answer = new string(text.Select(i => i == 'a' ? '1' : i == 'e' ? '2' :
+                                                      i == 'i' ? '3' : i == 'o' ? '4' :
+                                                      i == 'u' ? '5' : i).ToArray());
+            Console.WriteLine(answer);
         }
     }
 }
