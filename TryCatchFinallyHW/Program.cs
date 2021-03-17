258afa04e953e4000abda6465ff5553db272df57
unknown <nellysherova@mail.ru>
2021-03-17 18:15:18 +0300

    Добавила решение 2 таска

--- TryCatchFinallyHW/Program.cs
+++ TryCatchFinallyHW/Program.cs
@@ -10,10 +10,19 @@ namespace TryCatchFinallyHW
             Console.WriteLine("Task 1");
             Console.ResetColor();
             Console.Write("Enter text: "); string text = Console.ReadLine();
-            var answer = new string(text.Select(i => i == 'a' ? '1' : i == 'e' ? '2' :
-                                                      i == 'i' ? '3' : i == 'o' ? '4' :
-                                                      i == 'u' ? '5' : i).ToArray());
-            Console.WriteLine(answer);
+            var selectedText = new string(text.Select(t => t == 'a' ? '1' : t == 'e' ? '2' :
+                                                      t == 'i' ? '3' : t == 'o' ? '4' :
+                                                      t == 'u' ? '5' : t).ToArray());
+            Console.WriteLine(selectedText);
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Task 2");
+            Console.ResetColor();
+            Console.Write("Enter text: "); string text2 = Console.ReadLine();
+            var selectedText2 = new string(text2.Select(t => t == '1' ? 'a' : t == '2' ? 'e' :
+                                                      t == '3' ? 'i' : t == '4' ? 'o' :
+                                                      t == '5' ? 'u' : t).ToArray());
+            Console.WriteLine(selectedText2);
         }
     }
 }
